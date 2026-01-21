97ebcf06ebb86165fccf14bdbad6e922a7f7bd40
stefanos-vlachos <stevenvlaxos@gmail.com>
2026-01-21 19:08:17 +0200

    (Issue #39) Added PaSST tests

--- tests/test_deepaudiox.py
+++ tests/test_deepaudiox.py
@@ -121,6 +121,16 @@ def test_beats_backbone(input_tensor):
     assert output.shape == (1, 48, 768)
 
 
+@pytest.mark.parametrize("input_tensor", [torch.randn(1, 32000)])
+def test_passt_backbone(input_tensor):
+    passt = BACKBONES["passt"]()
+
+    output = passt.forward_pipeline(input_tensor)
+    print(output.shape)
+
+    assert output.shape == (1, 108, 768)
+
+
 @pytest.mark.parametrize("input_tensor", [torch.randn(1, 768)])
 @pytest.mark.parametrize("hidden_layers", [None, [128], [512, 128]])
 def test_mlp_head(input_tensor, hidden_layers):
@@ -132,7 +142,7 @@ def test_mlp_head(input_tensor, hidden_layers):
 
 @pytest.mark.parametrize("input_tensor", [torch.randn(1, 16000)])
 @pytest.mark.parametrize("num_classes", [2, 8])
-@pytest.mark.parametrize("backbone", ["beats"])
+@pytest.mark.parametrize("backbone", ["beats", "passt"])
 @pytest.mark.parametrize("pooling", ["simpool", "gap", "ep"])
 @pytest.mark.parametrize("freeze_backbone", [True, False])
 @pytest.mark.parametrize("pretrained", [True, False])
