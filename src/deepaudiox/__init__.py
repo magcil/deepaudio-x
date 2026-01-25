0b83e4d9833c6c9f4767d83a2072811de3a20e48
ChrisNick92 <chrisnick92@gmail.com>
2026-01-25 18:47:08 +0200

    (issue #50) export main api functionalities in __init__.py

--- src/deepaudiox/__init__.py
+++ src/deepaudiox/__init__.py
@@ -8,3 +8,36 @@ This package provides modules for:
 """
 
 __version__ = "0.1.5"
+
+# Top-level API exports
+from deepaudiox.datasets.audio_classification_dataset import (  # noqa: F401
+    AudioClassificationDataset,
+    audio_classification_dataset_from_dictionary,
+    audio_classification_dataset_from_dir,
+)
+from deepaudiox.loops.evaluator import Evaluator  # noqa: F401
+from deepaudiox.loops.trainer import Trainer  # noqa: F401
+from deepaudiox.modules.backbones import BACKBONES  # noqa: F401
+from deepaudiox.modules.constructors import (  # noqa: F401
+    AudioClassifierConstructor,
+    BackboneConstructor,
+)
+from deepaudiox.modules.pooling import POOLING  # noqa: F401
+
+# User-friendly aliases
+AudioClassifier = AudioClassifierConstructor
+Backbone = BackboneConstructor
+
+__all__ = [
+    "AudioClassifier",
+    "AudioClassifierConstructor",
+    "AudioClassificationDataset",
+    "Backbone",
+    "BackboneConstructor",
+    "Evaluator",
+    "Trainer",
+    "BACKBONES",
+    "POOLING",
+    "audio_classification_dataset_from_dictionary",
+    "audio_classification_dataset_from_dir",
+]
