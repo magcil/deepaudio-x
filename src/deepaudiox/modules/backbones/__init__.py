74d6c0e372fd22fc4a2c566630f354e53d79e6c4
ChrisNick92 <chrisnick92@gmail.com>
2026-03-20 09:41:49 +0200

    (issue #56) change function names monilenet -> mobilenet in backbones module

--- src/deepaudiox/modules/backbones/__init__.py
+++ src/deepaudiox/modules/backbones/__init__.py
@@ -36,18 +36,18 @@ def passt_base() -> PaSST:
 
 
 @register_backbone("mobilenet_05_as")
-def monilenet_05_base() -> MobileNet:
+def mobilenet_05_base() -> MobileNet:
     """MobileNet backbone"""
     return MobileNet(cfg=MobileNetConfig({"width_mult": 0.5}))
 
 
 @register_backbone("mobilenet_10_as")
-def monilenet_10_base() -> MobileNet:
+def mobilenet_10_base() -> MobileNet:
     """MobileNet backbone"""
     return MobileNet(cfg=MobileNetConfig({"width_mult": 1}))
 
 
 @register_backbone("mobilenet_40_as")
-def monilenet_40_base() -> MobileNet:
+def mobilenet_40_base() -> MobileNet:
     """MobileNet backbone"""
     return MobileNet(cfg=MobileNetConfig({"width_mult": 4}))
